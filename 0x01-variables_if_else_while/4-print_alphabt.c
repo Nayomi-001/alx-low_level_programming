58171464b0b001e5a5e4d91447420e57230cf82c
Naomi <omizach@gmail.com>
2022-10-22 12:29:30 -0700

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * 
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
